54a291d008f0b36291c1de53047006c90e2a4385
DuoX371 <62107810+DuoX371@users.noreply.github.com>
2021-05-07 04:41:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,7 @@
 <style>
 body {
   padding: 25px;
-  background-color: white;
-  color: black;
-  font-size: 25px;
-  transition: 0.5s;
+  transition: 0.2s;
 }
 
 * {
@@ -56,22 +53,103 @@ form.example::after {
   background-color: #272524;
   color: white;
 }
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
 </style>
 </head>
+<script>
+</script>
 <body>
   <img src="https://cdn.discordapp.com/emojis/720984944406429837.png?v=1"></img>
-  <button onclick="myFunction()" style="margin-left:90%">Toggle dark mode</button>
+
+
+
+  <label for="darkmode" class="switch" style="margin-left:90%;">
+  <input type="checkbox" id="darkmode">
+  <span class="slider round"></span>
+  </label>
+
+
+
 <form class="example" action="" style="margin:auto;max-width:300px">
   <input type="text" placeholder="Search manga.." name="manga">
   <button type="submit"><i class="fa fa-search"></i></button>
 </form>
 
+<script>
+var checkbox = document.getElementById("darkmode");
+var element = document.body;
+checkbox.addEventListener("change", function() {
+  localStorage.setItem("dark-mode",this.checked);
+  if (checkbox.checked) {
+    element.classList.add("dark-mode");
+  } else {
+    element.classList.remove("dark-mode");
+  }
+});
+
+</script>
+
+
 </body>
 
-<script>
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-}
-</script>
 </html>
