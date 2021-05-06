dc7a1813d72fa2da81cb99f81de6c58426e15e40
Yewon Kim <sahrularsad@gmail.com>
2021-05-07 04:52:23 +0800

    Update Not Found Fix

--- mangadexapi.php
+++ mangadexapi.php
@@ -3,10 +3,14 @@ if(!empty($_POST['manga'])){
 	$query = $_POST['manga'];
 	$json = file_get_contents("https://api.mangadex.org/manga?limit=100&title={$query}");
 	$result = json_decode($json);
-	foreach($result->results as $resultss){
-		$id = $resultss->data->id;
-		$html =  "<a href='manga?id={$id}'>" . $resultss->data->attributes->title->en . "</a><br>";
-		echo $html;
+	if($result != ""){
+		foreach($result->results as $resultss){
+			$id = $resultss->data->id;
+			$html =  "<a href='manga?id={$id}'>" . $resultss->data->attributes->title->en . "</a><br>";
+			echo $html;
+		}
+	}else{
+		echo "Manga Not Found!";
 	}
 }else{
 	echo 0;
