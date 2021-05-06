e70f3a4e7fc05c334afcd309f9f75162405fb1ea
Yewon Kim <sahrularsad@gmail.com>
2021-05-07 05:19:35 +0800

    Update mangadexapi.php

--- mangadexapi.php
+++ mangadexapi.php
@@ -9,13 +9,39 @@ if(!empty($_POST['manga'])){
 		foreach($result->results as $resultss){
 			if($i == 1){
 				$id = $resultss->data->id;
-				$html .=  "<tr><td><a href='manga?id={$id}'>" . $resultss->data->attributes->title->en . "</a></td>";
+				$html .=  "<tr><td><a href='mangainfo.php?id={$id}'>" . $resultss->data->attributes->title->en . "</a><br>(" . $resultss->data->attributes->status . ")</td>";
 			}else if($i == 2){
 				$id = $resultss->data->id;
-				$html .=  "<td><a href='manga?id={$id}'>" . $resultss->data->attributes->title->en . "</a></td>";
+				$html .=  "<td><a href='mangainfo.php?id={$id}'>" . $resultss->data->attributes->title->en . "</a><br>(" . $resultss->data->attributes->status . ")</td>";
 			}else{
 				$id = $resultss->data->id;
-				$html .=  "<td><a href='manga?id={$id}'>" . $resultss->data->attributes->title->en . "</a></td></tr>";
+				$html .=  "<td><a href='mangainfo.php?id={$id}'>" . $resultss->data->attributes->title->en . "</a><br>(" . $resultss->data->attributes->status . ")</td></tr>";
+				$i = 0;
+			}
+			$i++;
+		}
+		$html .= "</table>";
+		echo $html;
+	}else{
+		echo "Manga Not Found!";
+	}
+}else if(!empty($_GET['id'])){
+	$query = $_GET['id'];
+	$json = file_get_contents("https://api.mangadex.org/manga?limit=100&title={$query}");
+	$result = json_decode($json);
+	if($result != ""){
+		$i = 1;
+		$html = "<table>";
+		foreach($result->results as $resultss){
+			if($i == 1){
+				$id = $resultss->data->id;
+				$html .=  "<tr><td><a href='mangainfo.php?id={$id}'>" . $resultss->data->attributes->title->en . "</a><br>(" . $resultss->data->status . ")</td>";
+			}else if($i == 2){
+				$id = $resultss->data->id;
+				$html .=  "<td><a href='mangainfo.php?id={$id}'>" . $resultss->data->attributes->title->en . "</a>(" . $resultss->data->attributes->status . ")</td>";
+			}else{
+				$id = $resultss->data->id;
+				$html .=  "<td><a href='mangainfo.php?id={$id}'>" . $resultss->data->attributes->title->en . "</a>(" . $resultss->data->attributes->status . ")</td></tr>";
 				$i = 0;
 			}
 			$i++;
